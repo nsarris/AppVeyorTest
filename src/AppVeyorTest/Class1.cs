51358ea590dba07924146984ff0ef3605d6d209c
nsarris <nsarris@gmail.com>
2019-09-22 14:43:55 +0300

    Division hotfix

--- src/AppVeyorTest/Class1.cs
+++ src/AppVeyorTest/Class1.cs
@@ -6,6 +6,6 @@ namespace AppVeyorTest
     {
         public int Add(int x, int y) => x + y;
         public int Subtract(int x, int y) => x - y;
-        public double Divide(int x, int y) => x / y;
+        public double Divide(int x, int y) => (double)x / (double)y;
     }
 }
